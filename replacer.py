67d2dfd11892064bae146e85edfc992afd615ec8
Alexander <alexsander@proxsis.net>
2024-09-05 11:38:57 -0300

    replacer to delphi files

---
+++ replacer.py
@@ -0,0 +1,55 @@
+from pathlib import Path
+import chardet
+
+parse = {
+    'Ã­-': 'í',
+    'Ã§': 'ç',
+    'Ã£': 'ã',
+    'Ã³': 'á',
+    'Ã©': 'é',
+    'Ãª': 'ê'
+}
+
+def detect_encoding(file_path):
+    with open(file_path, 'rb') as file:
+        detector = chardet.universaldetector.UniversalDetector()
+        for line in file:
+            detector.feed(line)
+            if detector.done:
+                break
+        detector.close()
+    return detector.result['encoding']
+
+
+def inParse(string: str):
+    for key, value in parse.items():
+        if value in string:
+            find = string.find(value)
+            print(key)
+            print(find)
+            print(string[find:find+25])
+            return True
+
+path = Path("C:\\fontes\\proxsis\\promanager\\aplicativo\\modulos")
+for caminho in path.iterdir():
+    if caminho.is_dir():
+        children = Path(caminho)
+        for child in children.iterdir():
+            if str(child)[-4:] == '.pas':
+                try:
+                    enco = detect_encoding(child)
+                    if str(enco) != 'UTF-8-SIG':
+                        print(f"{child} -> {enco}")
+                        with open(child, 'r', encoding=enco) as file:
+                            data = file.read()
+                            if inParse(data):
+                                for key, value in parse.items():
+                                    data = data.replace(str(key), str(value))
+                                data.encode('UTF-8-SIG')
+                                with open(child, 'w', encoding='UTF-8-SIG') as txt:
+                                    txt.write(data)
+                                
+                except Exception as e:
+                    print(f'Erro {e} no arquivo {child}')
+    else:
+        continue
